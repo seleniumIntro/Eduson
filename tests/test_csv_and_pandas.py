7908a5a7de91ddcc297d9e6de7384c680ffaedbd
Alexander <alexander.dobychin@foodrocket.me>
2023-04-05 20:12:37 +0300

    just_start

--- tests/test_csv_and_pandas.py
+++ tests/test_csv_and_pandas.py
@@ -5,23 +5,30 @@ from pyparsing import Word, alphas
 
 class TestCsv:
     def test_csv(self):
+        #указываем путь до файла и открываем его как объект
         with open('../configs/account.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
+            #превращаем объект в массив строк
+            date = csv.reader(csvfile)
+            #распечатываем каждую строку в цикле
+            for row in date:
                 print(', '.join(row))
 
     def test_pandas_sort(self):
-
+        #создаём словарь с данными
         data = {'Clients': ['Oleg','Viktor','Maria','Andrey'],
                 'Money': [44,25000,24700000,350000]
                 }
-        df = pandas.DataFrame(data)
+        df = pandas.DataFrame(data) #закидываем данные в датафрейм
+        #задаём колонку для сортировки, и тип inplace=True - по возрастанию
         df.sort_values(by=['Money'], inplace=True)
-
+        #распечатываем отсортированный датафрейм
         print(df)
 
     def test_pyparsing(self):
+        #Создаём шаблон для парсинга
         greet = Word(alphas) + "," + Word(alphas) + "!"
+        #создаём строку, слова которой хотим разложить по шаблону
+        #и добавить в массив
         hello = "Hello, World!"
         print(hello, "->", greet.parseString(hello))
 
--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -1,39 +1,37 @@
-import allure
 import pytest
 
 from configs.config import GlobalConfig
+from pages.pages_amazon import PagesAmazon
+from pages.pages_gis import PagesGis
 from pages.pages_main import PagesMain
 
 
-@pytest.mark.xdist_group("group1")
+@pytest.mark.xdist_group("group1") #декоратор для параллезации
 class TestParallel:
 
+    #первый тест для параллельного запуска
     def test_positive_login_mail_ru(self, driver, configuration: GlobalConfig):
         pages_main = PagesMain(driver, 'https://mail.ru/')
         pages_main.open()
-        with allure.step('Login in mail.ru'):
-            pages_main.login(configuration.login, configuration.password)
-        with allure.step('Get email text'):
-            pages_main.get_login()
-        with allure.step('Check email text'):
-            assert configuration.login == pages_main.get_login()
+        #Login in mail.ru
+        pages_main.login(configuration.login, configuration.password)
+        #Get email text
+        pages_main.get_login()
+        #Check email text
+        assert configuration.login == pages_main.get_login()
 
-    def test_positive_login_mail_ru_second(self, driver, configuration: GlobalConfig):
-        pages_main = PagesMain(driver, 'https://mail.ru/')
-        pages_main.open()
-        with allure.step('Login in mail.ru'):
-            pages_main.login(configuration.login, configuration.password)
-        with allure.step('Get email text'):
-            pages_main.get_login()
-        with allure.step('Check email text'):
-            assert configuration.login == pages_main.get_login()
+    #второй тест для параллельного запуска
+    def test_set_snow(self, driver, configuration: GlobalConfig):
+        pages_main = PagesGis(driver, 'https://www.gismeteo.ru/')
+        pages_main.set_tomorrow()
+        pages_main.set_snow()
 
-    def test_positive_login_mail_ru_third(self, driver, configuration: GlobalConfig):
-        pages_main = PagesMain(driver, 'https://mail.ru/')
-        pages_main.open()
-        with allure.step('Login in mail.ru'):
-            pages_main.login(configuration.login, configuration.password)
-        with allure.step('Get email text'):
-            pages_main.get_login()
-        with allure.step('Check email text'):
-            assert configuration.login == pages_main.get_login()
+    #третий тест для параллельного запуска
+    def test_login_in_amazon(self, driver, configuration: GlobalConfig):
+        email = 'selenium.introduction@mail.ru'
+        password = 123456
+        pages_amazon = PagesAmazon(driver, "https://www.amazon.com/")
+        pages_amazon.open()
+        pages_amazon.login(email, password)
+        assert "Hello, test" == pages_amazon.get_login()
+
